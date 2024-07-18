d30045b39676ea4f66030472ed9df5ba95bc8645
Seunghyun Kim <tmdgus8779@gmail.com>
2024-07-18 12:18:22 +0900

    [Silver I] Title: 스티커, Time: 740 ms, Memory: 47740 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    d = [list(map(int, input().split())) for _ in range(2)]
+
+    if n > 1:
+        d[0][1] += d[1][0]
+        d[1][1] += d[0][0]
+
+    for i in range(2, n):
+        d[0][i] += max(d[1][i - 1], d[1][i - 2])
+        d[1][i] += max(d[0][i - 1], d[0][i - 2])
+
+    print(max(d[0][n - 1], d[1][n - 1]))
