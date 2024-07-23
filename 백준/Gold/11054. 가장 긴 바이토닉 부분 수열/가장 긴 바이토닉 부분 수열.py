c4e918efee99370d83de2413ad2601e1ff277b76
Seunghyun Kim <tmdgus8779@gmail.com>
2024-07-23 20:45:19 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 268 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+inc = [1] * n
+dec = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            inc[i] = max(inc[i], inc[j] + 1)
+
+for i in range(n - 1, -1, -1):
+    for j in range(n - 1, i, -1):
+        if arr[i] > arr[j]:
+            dec[i] = max(dec[i], dec[j] + 1)
+
+result = []
+for i in range(n):
+    result.append(inc[i] + dec[i] - 1)
+
+print(max(result))
