b5c2b172155ba4d3df78635d2c7df1d6080d3e02
Seunghyun Kim <tmdgus8779@gmail.com>
2025-01-04 23:17:19 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+x = int(input())
+n = int(input())
+
+money = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    money += a * b
+
+if money == x:
+    print("Yes")
+else:
+    print("No")
