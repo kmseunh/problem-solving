6e90eefc7cef42ead830231feaae40c60bc0e582
Seunghyun Kim <tmdgus8779@gmail.com>
2025-01-05 23:19:47 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+arr = list(range(n + 1))
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i]
+
+print(" ".join(map(str, arr[1:])))
